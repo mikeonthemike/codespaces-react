370a060563750ddbcf9d2d5fbe5e49b51ac1cbac
mikeonthemike <michael_spragg@hotmail.com>
2023-04-02 04:55:52 +0000

    Updated app to create initial drawing functions

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      
+      <canvas id="myCanvas" width="400" height="400"></canvas>
+      <script src="random-lines.js"></script>
+      <script>
+        const canvas = document.getElementById("myCanvas");
+        drawRandomLines(canvas, 10); // draws 10 random lines on the canvas
+      </script>
+
       </header>
     </div>
   );
 }
+
+// const canvas = document.getElementById("canvas");
+// drawRandomLines(canvas, 10); // draws 10 random lines on the canvas
+
+
+function drawRandomLines(canvas, numLines) {
+  const ctx = canvas.getContext("2d");
+
+  for (let i = 0; i < numLines; i++) {
+    const x1 = Math.floor(Math.random() * canvas.width);
+    const y1 = Math.floor(Math.random() * canvas.height);
+    const x2 = Math.floor(Math.random() * canvas.width);
+    const y2 = Math.floor(Math.random() * canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+  }
+}
+
 
 export default App;
